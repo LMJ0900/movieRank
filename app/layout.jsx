707533c69083d61371c5a34973f8bbbd7dca8b6a
LeeMinjae <LMJ0900@github.com>
2025-04-24 17:04:33 +0900

    g태그 설치

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,6 @@
 import "./globals.css";
+import Script from "next/script";
+
 export const metadata = {
   title: "movie",
   description: "영화소개 페이지",
@@ -7,7 +9,30 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag Manager (JS) */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;
+            j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+            f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PF7BF4VX');
+          `}
+        </Script>
+      </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PF7BF4VX"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         {children}
       </body>
     </html>
