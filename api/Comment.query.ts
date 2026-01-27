c007dc72e403e5702d177b12a8b76ff04c917d42
LeeMinjae <LMJ0900@github.com>
2026-01-27 17:08:11 +0900

    [Feature] 내 좋아요 클릭 여부, 해당 영화 댓글의 모든 좋아요 쿼리

--- api/Comment.query.ts
+++ api/Comment.query.ts
@@ -16,4 +16,27 @@ export class CommentQuery {
         if (error) throw error;
         return (data ?? []) as CommentRowType[];
     }
+
+    static async getLikesByIds(ids: number[]){
+        if (ids.length === 0) return [];
+        const { data, error } = await supabase
+          .from('likes')
+          .select('comment_id') // 최소 컬럼
+          .in('comment_id', ids);
+        
+        if (error) throw error;
+        return data ?? [];
+    }
+
+    static async getMyLikesByCommentIds(userId: string, ids: number[]) {
+    if (ids.length === 0) return [];
+    const { data, error } = await supabase
+      .from('likes')
+      .select('comment_id')
+      .eq('user_id', userId)
+      .in('comment_id', ids);
+
+    if (error) throw error;
+    return data ?? [];
+  }
 }
